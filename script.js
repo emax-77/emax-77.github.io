6ef50df4029ca197eaf8e04bb09b2f332bf4621b
Peter Wirth <57559346+emax-77@users.noreply.github.com>
2024-08-28 14:43:35 +0200

    update script.jss

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
     const yearSpan = document.createElement('span');
     yearSpan.textContent = new Date().getFullYear();
-    document.querySelector('footer p').append(` ${yearSpan.textContent}`);
 });
