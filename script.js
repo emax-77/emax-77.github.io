4a9a3047d9a45f72f7cd9879ada0eb31e5ce5689
Peter Wirth <57559346+emax-77@users.noreply.github.com>
2024-09-17 18:37:03 +0200

    big style update on index.html, script.js and style.css

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const year = new Date().getFullYear();  
-    document.getElementById('current-year').textContent = year;  
-});
+// Scroll to top button
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+scrollToTopBtn.onclick = function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// Set current year in footer
+document.getElementById("current-year").textContent = new Date().getFullYear();
